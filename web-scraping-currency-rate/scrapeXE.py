1f60ecc62ecd749caa3f139afaa75bf72be6384b
rishijd <rishijd@gmail.com>
2018-01-09 22:25:38 -0500

    Create scrapeXE.py
    Python file to practice web scraping and do something unique (yet basic) since we already scraped Amazon through learning exercises.

---
+++ web-scraping-currency-rate/scrapeXE.py
@@ -0,0 +1,34 @@
+'''
+Script to fetch the currency exchange rate of GBP-USD from xe.com,
+with pseudocode below for further updates I am interested in being alerted of the rate if it goes above a certain threshold.
+This is a base web-scraping script, following a workshop I attended on Python and web scraping, where we scraped Amazon and a PDF.
+(For Amazon, we fetched a product price and then compared it (on a basic level) to other "people also viewed" products in the same category, to find the lowest price.
+'''
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+url = "http://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=USD" # Currency rate GBP-USD
+page = requests.get(url, headers=headers)
+bsobj = BeautifulSoup(page.content, "html.parser")
+
+rate = bsobj.find("span",{"class":"uccResultAmount"}) # returns the full <span> tag with lots of whitepsace inside from Amazon. This is an object, not a string.
+print(rate.get_text()) # prints the rate
+
+# I actually want to be notified if the rate goes above a certain threshold, in case I want to trade in the currency at the rate.
+# Here is the pseudocode:
+
+'''
+Pseudocode:
+
+    threshold = {get value from database}
+    last-email-send = {get value from database, to tell when we last sent an email from this script}
+    
+    if (rate <= threshold and last-email-send + 12 hours <= now):
+        send email to me with a reminder, or send alert via API of preferred system for notification
+        store last-email-send date in database, so that we won't spam the recipient!
+    
+Notes:
+    We need to ensure this is run as a cron every hours or 'x' hours depending on how regularly we want to check the exchange rate
+'''
